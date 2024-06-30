2023ad1345e17b862ad9fe769cf00b1d7f0643c0
y-kamiya <y.kamiya0@gmail.com>
2024-06-30 20:39:20 +0900

    profile 2

--- apps/run.py
+++ apps/run.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
             dataset = mv.RandomMultiviewCameraIterableDataset(config)
             return DataLoader(dataset, batch_size=4, num_workers=0, collate_fn=dataset.collate)
         else:
-            size = 4 if phase == 'val' else 1
+            size = 4 if phase == 'val' else 8
             dataset = ViewDataset(cfg.data, device=device, type=phase, size=size)
             return DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
 
