b05a3d30ef289ae1964e0531ca355b60faf1b77e
y-kamiya <y.kamiya0@gmail.com>
2024-10-07 19:35:24 +0900

    fix

--- scripts/adjust_camera.py
+++ scripts/adjust_camera.py
@@ -1,64 +1,91 @@
+import argparse
+import time
 import torch
-from PIL import Image
 import torch.nn.functional as F
-from torchvision import transforms
-from torchvision.utils import save_image
+import torchvision
+from PIL import Image
+from pathlib import Path
 
 from lib.provider import ZoomOutViewDataset
 from lib.dlmesh import DLMesh
 from lib.common.utils import load_config
 
-import time
+
+def main(args):
+    start = time.time()
+
+    device = torch.device("cuda")
+    radius_list = torch.arange(args.radius_min, args.radius_max, args.radius_step)
+    center_diff_list = torch.arange(args.center_min, args.center_max, args.center_step) if args.face else torch.tensor([0])
+    H = args.height
+    W = args.width
+
+    cfg = load_config('configs/default.yaml')
+    model = DLMesh(cfg.model).eval().to(device)
+    face_center, face_scale = model.get_mesh_center_scale("face")
+
+    dataset = ZoomOutViewDataset(cfg.data, device=device, radius_list=radius_list, is_full_body=not args.face)
+    dataset.face_scale = face_scale.item()
+    dataloder = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
+
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize((H, W)),
+        torchvision.transforms.ToTensor()
+    ])
+
+    image = Image.open(args.image_path)
+    image = transforms(image)
+    alpha_ref = image[3:, :, :].unsqueeze(0).to(device)
+
+    min_loss = 1e9
+    best_radius = -1.0
+    best_center = None
+    best_alpha = None
+    for center_diff in center_diff_list:
+        if args.face:
+            center = face_center.clone().detach()
+            center[1] += center_diff
+            dataset.face_center = center
+        for i, data in enumerate(dataloder):
+            with torch.no_grad():
+                out = model(data["rays_o"], data["rays_d"], data["mvp"], H, W, shading="albedo")
+            alpha = out['alpha'].permute(0, 3, 1, 2)
+
+            loss = F.mse_loss(alpha, alpha_ref).item()
+            radius = radius_list[i].item()
+            diff = center_diff.item()
+            if loss < min_loss:
+                min_loss = loss
+                best_radius = radius
+                best_center = diff
+                best_alpha = alpha
+
+            print(f"{radius:.3f}, center: {diff:.4f}, loss: {loss:.4f}")
+            # output = torch.cat([alpha_ref, alpha])
+            # torchvision.utils.save_image(output, f"tmp/{diff:.3f}_{radius:.2f}.jpg")
+
+        print("---")
+
+    print(f"radius: {best_radius:.3f}, center: {best_center:.4f}, loss: {min_loss:.4f}")
+    output = torch.cat([alpha_ref, best_alpha])
+    torchvision.utils.save_image(output, "best_alpha.jpg")
+
+    print(time.time() - start)
 
 
-start = time.time()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument("image_path", type=Path)
+    parser.add_argument("--face", action="store_true")
+    parser.add_argument("--width", type=int, default=512)
+    parser.add_argument("--height", type=int, default=512)
+    parser.add_argument("--radius_min", type=float, default=0.5)
+    parser.add_argument("--radius_max", type=float, default=1.6)
+    parser.add_argument("--radius_step", type=float, default=0.1)
+    parser.add_argument("--center_min", type=float, default=-0.04)
+    parser.add_argument("--center_max", type=float, default=0.05)
+    parser.add_argument("--center_step", type=float, default=0.01)
+    args = parser.parse_args()
+    print(args)
 
-device = torch.device("cuda")
-H, W = 1024, 1024
-radius_list = torch.arange(0.5, 1.5, 0.1)
-center_diff_list = torch.arange(-0.03, 0.03, 0.005)
-is_full_body = False
-
-cfg = load_config('configs/default.yaml')
-model = DLMesh(cfg.model).eval().to(device)
-face_center, face_scale = model.get_mesh_center_scale("face")
-
-dataset = ZoomOutViewDataset(cfg.data, device=device, radius_list=radius_list, is_full_body=is_full_body)
-dataset.face_scale = face_scale.item()
-dataloder = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=False, num_workers=0)
-
-transforms = transforms.Compose([
-    transforms.Resize((H, W)),
-    transforms.ToTensor()
-])
-image_path = "data/images/unitychan_a_e0/0.png" if is_full_body else "data/images/unitychan_face_rgba.png"
-image = Image.open(image_path)
-image = transforms(image)
-alpha_ref = image[3:, :, :].to(device)
-
-min_loss = 1e9
-best_radius = -1.0
-best_center = None
-best_alpha = None
-for center_diff in center_diff_list:
-    center = face_center.clone().detach()
-    center[1] += center_diff
-    dataset.face_center = center
-    for i, data in enumerate(dataloder):
-        with torch.no_grad():
-            out = model(data["rays_o"], data["rays_d"], data["mvp"], H, W, shading="albedo")
-        alpha = out['alpha'].permute(0, 3, 1, 2)
-
-        loss = F.mse_loss(alpha, alpha_ref).item()
-        if loss < min_loss:
-            min_loss = loss
-            best_radius = radius_list[i]
-            best_center = center_diff
-            best_alpha = alpha
-
-        print(i, radius_list[i], center_diff, loss)
-
-print(best_radius, best_center, min_loss)
-save_image(best_alpha, "best_alpha.jpg")
-
-print(time.time() - start)
+    main(args)
